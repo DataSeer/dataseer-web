42c6b9a5cde84de717e4359fe92800a5435ef77e
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2020-03-30 18:15:26 +0200

    add previous & next feature

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -5,8 +5,8 @@
 (function($) {
   // Get the current Object
   return MongoDB.getCurrentDocument(function(currentDocument) {
-    // On metadata_validation click
-    $('#demo_previous_step').click(function() {
+    // On previous click
+    $('#previous_step').click(function() {
       return MongoDB.getCurrentDocument(function(currentDocument) {
         currentDocument.status = MongoDB.getPreviousStatus(currentDocument);
         MongoDB.updateDocument(currentDocument, null, function(err, res) {
@@ -17,22 +17,8 @@
       });
     });
 
-    if (currentDocument.status === 'finish') {
-      // On metadata_validation click
-      $('#demo_switch_view').click(function() {
-        return MongoDB.getCurrentDocument(function(currentDocument) {
-          currentDocument.isDataSeer = !currentDocument.isDataSeer;
-          MongoDB.updateDocument(currentDocument, null, function(err, res) {
-            console.log(err, res);
-            if (err) return err; // Need to define error behavior
-            return location.reload();
-          });
-        });
-      });
-    }
-
-    // On metadata_validation click
-    $('#demo_next_step').click(function() {
+    // On next click
+    $('#next_step').click(function() {
       return MongoDB.getCurrentDocument(function(currentDocument) {
         currentDocument.status = MongoDB.getNextStatus(currentDocument);
         MongoDB.updateDocument(currentDocument, null, function(err, res) {
