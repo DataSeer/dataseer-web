c69758ce283a05c57b1b8c98a48d85536415872f
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2020-08-31 17:35:22 +0200

    fix issue https://github.com/DataSeer/dataseer-web/issues/91

--- public/javascripts/cookies.js
+++ public/javascripts/cookies.js
@@ -2,53 +2,25 @@
  * @prettier
  */
 
-window.cookieconsent.initialise({
-  'palette': {
-    'popup': {
-      'background': '#237afc'
+(function($) {
+  window.cookieconsent.initialise({
+    'palette': {
+      'popup': {
+        'background': '#237afc'
+      },
+      'button': {
+        'background': '#fff',
+        'text': '#237afc'
+      }
     },
-    'button': {
-      'background': '#fff',
-      'text': '#237afc'
+    'theme': 'classic',
+    'content': {
+      'message': 'By using this app, you consent to our cookie policy',
+      'dismiss': 'Dismiss',
+      'allow': 'Accept',
+      'deny': 'Deny',
+      'link': 'Learn more',
+      'href': 'privacy'
     }
-  },
-  'theme': 'classic',
-  'type': 'opt-in',
-  'content': {
-    'message': 'Custom message',
-    'dismiss': 'Dismiss',
-    'allow': 'Accept',
-    'deny': 'Deny',
-    'link': 'Learn more',
-    'href': 'privacy'
-  },
-  'onInitialise': function(status) {
-    var type = this.options.type;
-    var didConsent = this.hasConsented();
-    if (type == 'opt-in' && didConsent) {
-      // enable cookies
-    }
-    if (type == 'opt-out' && !didConsent) {
-      // disable cookies
-    }
-  },
-  'onStatusChange': function(status, chosenBefore) {
-    var type = this.options.type;
-    var didConsent = this.hasConsented();
-    if (type == 'opt-in' && didConsent) {
-      // enable cookies
-    }
-    if (type == 'opt-out' && !didConsent) {
-      // disable cookies
-    }
-  },
-  'onRevokeChoice': function() {
-    var type = this.options.type;
-    if (type == 'opt-in') {
-      // disable cookies
-    }
-    if (type == 'opt-out') {
-      // enable cookies
-    }
-  }
-});
+  });
+})(jQuery);
