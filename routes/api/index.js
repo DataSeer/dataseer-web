1777e9e90c2b800e9ee0a6882766e372acd9b5ee
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2021-09-06 08:09:19 +0200

    fix : fix cookies expires attr

--- routes/api/index.js
+++ routes/api/index.js
@@ -128,7 +128,7 @@ router.get(`/signout`, function (req, res, next) {
     let result = isError ? data.toString() : data;
     // Unset cookie (useful for navigator)
     if (!isError)
-      res.setHeader(`Set-Cookie`, cookie.serialize(`token`, ``, { httpOnly: true, path: `/`, expires: Date.now() }));
+      res.setHeader(`Set-Cookie`, cookie.serialize(`token`, ``, { httpOnly: true, path: `/`, expires: new Date() }));
     return res.json({ err: null, res: true });
   });
 });
