c3850ffe3728b7ff41748750661b51cd63fd78d4
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2021-09-29 00:49:10 +0200

    update: update access rights of finish step features

--- routes/api/documents.js
+++ routes/api/documents.js
@@ -939,7 +939,7 @@ router.post(`/:id/datasets/backToMetadata`, function (req, res, next) {
 /* Validate datasets of a given document */
 router.post(`/:id/finish/reopen`, function (req, res, next) {
   let accessRights = AccountsManager.getAccessRights(req.user);
-  if (!accessRights.authenticated) return res.status(401).send(conf.errors.unauthorized);
+  if (!accessRights.isModerator) return res.status(401).send(conf.errors.unauthorized);
   let opts = {
     data: {
       id: req.params.id
