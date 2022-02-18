89ebf4e2c8fe49cfc707b9f2b1fbdc900c937b1a
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2022-02-18 02:05:28 +0100

    update: add sciscore results API route

--- routes/api/sciscore.js
+++ routes/api/sciscore.js
@@ -4,8 +4,13 @@
 
 'use strict';
 
+const fs = require(`fs`);
+const path = require(`path`);
+
 const express = require(`express`);
 const router = express.Router();
+
+const archiver = require(`archiver`);
 
 const DocumentsController = require(`../../controllers/api/documents.js`);
 const DocumentsFilesController = require(`../../controllers/api/documents.files.js`);
@@ -33,6 +38,42 @@ router.post(`/processFile/:id`, function (req, res, next) {
       return Sciscore.processFile({ filePath: filePath }, function (err, body) {
         if (err) return next(err);
         else return res.json(body);
+      });
+    });
+  });
+});
+
+router.get(`/results/:id`, function (req, res, next) {
+  let accessRights = AccountsManager.getAccessRights(req.user);
+  if (!accessRights.isAdministrator) return res.status(401).send(conf.errors.unauthorized);
+  return DocumentsController.get({ data: { id: req.params.id }, user: req.user }, function (err, doc) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(conf.errors.internalServerError);
+    }
+    if (!doc || doc instanceof Error) return res.status(404).send(conf.errors.notFound);
+    let fileId = doc.pdf ? doc.pdf : doc.tei;
+    return DocumentsFilesController.getFilePath({ data: { id: fileId.toString() } }, function (err, filePath) {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(conf.errors.internalServerError);
+      }
+      let dirname = path.dirname(filePath);
+      let sciscoreDirectory = path.join(dirname, `sciscore`);
+      return fs.stat(sciscoreDirectory, function (err, stats) {
+        if (!stats.isDirectory()) return res.status(404).send(`Sciscore results not found`);
+        res.setHeader(`Content-Type`, `application/zip`);
+        res.setHeader(`Content-disposition`, `attachment; filename=sciscore.zip`);
+        let zip = archiver(`zip`);
+        zip.on(`error`, function (err) {
+          console.log(err);
+          throw err;
+        });
+        // Send the file to the express result stream
+        zip.pipe(res);
+        // Add all files of the sciscore directory into zip archive
+        zip.directory(sciscoreDirectory, false);
+        zip.finalize();
       });
     });
   });
