7964ba800356ce85c9dee201c74ae51952e5f89a
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2022-02-27 04:58:56 +0100

    update: add 'ORCID API' feature

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ const repoRecommenderRouter = require(`./routes/api/repoRecommender.js`);
 const softciteRouter = require(`./routes/api/softcite.js`);
 const statisticsRouter = require(`./routes/api/statistics.js`);
 const hypothesisRouter = require(`./routes/api/hypothesis.js`);
+const orcidRouter = require(`./routes/api/orcid.js`);
 const sciscoreRouter = require(`./routes/api/sciscore.js`);
 const chartsRouter = require(`./routes/api/charts.js`);
 
@@ -237,6 +238,7 @@ db.once(`open`, function () {
       app.use(`/api/repoRecommender`, repoRecommenderRouter);
       app.use(`/api/statistics`, statisticsRouter);
       app.use(`/api/hypothesis`, hypothesisRouter);
+      app.use(`/api/orcid`, orcidRouter);
       app.use(`/api/sciscore`, sciscoreRouter);
       app.use(`/api/charts`, chartsRouter);
 
