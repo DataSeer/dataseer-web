f6113401a5e66aba8d8bb10337058b31bf19ceec
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2022-03-04 18:14:54 +0100

    update: add google drive save feature

---
+++ db-save/upload.js
@@ -0,0 +1,29 @@
+/*
+ * @prettier
+ */
+
+'use strict';
+
+const _ = require(`lodash`);
+const async = require(`async`);
+const path = require(`path`);
+
+const gDrive = require(`../lib/googleDrive.js`);
+
+const conf = require(`../conf/save.json`);
+
+const args = process.argv.slice(2);
+const fileName = args[0];
+const filePath = path.join(__dirname, fileName);
+const folder = conf.folder;
+
+console.log(`Sending .tar file ${filePath} to Google Drive folder: ${folder}`);
+
+gDrive.createSaveFile({ folder: folder, erase: true, data: { name: fileName, path: filePath } }, function (err, res) {
+  if (err) {
+    console.log(`An error has occurred:`);
+    console.log(err);
+  } else {
+    console.log(`Archive saved`);
+  }
+});
