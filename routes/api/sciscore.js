1a3b84dc3297e3d59fd135e16bd2a65615362aff
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2022-03-22 10:14:22 +0100

    update: update sciscore feature

--- routes/api/sciscore.js
+++ routes/api/sciscore.js
@@ -43,6 +43,29 @@ router.post(`/processFile/:id`, function (req, res, next) {
   });
 });
 
+router.post(`/processSentences/:id`, function (req, res, next) {
+  let accessRights = AccountsManager.getAccessRights(req.user);
+  if (!accessRights.isAdministrator) return res.status(401).send(conf.errors.unauthorized);
+  return DocumentsController.get({ data: { id: req.params.id }, user: req.user }, function (err, doc) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(conf.errors.internalServerError);
+    }
+    if (!doc || doc instanceof Error) return res.status(404).send(conf.errors.notFound);
+    let fileId = doc.tei;
+    return DocumentsFilesController.getFilePath({ data: { id: fileId.toString() } }, function (err, filePath) {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(conf.errors.internalServerError);
+      }
+      return Sciscore.processSentences({ filePath: filePath }, function (err, body) {
+        if (err) return next(err);
+        else return res.json(body);
+      });
+    });
+  });
+});
+
 router.get(`/results/:id`, function (req, res, next) {
   let accessRights = AccountsManager.getAccessRights(req.user);
   if (!accessRights.isAdministrator) return res.status(401).send(conf.errors.unauthorized);
