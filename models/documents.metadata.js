40d17ccf0b83c67f75dc719ec1d873b7bfefdcb7
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2022-05-20 15:31:00 +0200

    update: update models

--- models/documents.metadata.js
+++ models/documents.metadata.js
@@ -10,6 +10,7 @@ const Orcid = new mongoose.Schema({ fromTEI: [String], fromAPI: [Object] }, { _i
 
 const Author = new mongoose.Schema(
   {
+    isLeadSubmitting: Boolean,
     name: String,
     email: String,
     'family-name': String,
