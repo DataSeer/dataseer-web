c4d71a73e7081a69647cc07b5705cdf2deef567e
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2022-12-14 16:59:25 +0100

    update : disable upload feature for standard users

--- routes/backoffice/index.js
+++ routes/backoffice/index.js
@@ -16,7 +16,7 @@ const conf = require(`../../conf/conf.json`);
 
 router.get(`/upload`, function (req, res) {
   let accessRights = AccountsManager.getAccessRights(req.user, AccountsManager.match.all);
-  if (!accessRights.authenticated)
+  if (!accessRights.authenticated || accessRights.isStandardUser || accessRights.isVisitor)
     return res.redirect(Url.build(`/signin`, { unauthorized: true, redirect: req.originalUrl }));
   return res.render(`root/backoffice/upload/layout.pug`, {
     currentRoute: `/backoffice/upload`,
