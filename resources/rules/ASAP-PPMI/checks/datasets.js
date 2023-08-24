b8164a92263edc28f0e6b2bfe45b2f9598d48f90
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2023-08-24 17:26:54 +0200

    update : add 'Non PPMI' case in ASAP PPMI report

--- resources/rules/ASAP-PPMI/checks/datasets.js
+++ resources/rules/ASAP-PPMI/checks/datasets.js
@@ -8,6 +8,10 @@ let Self = {};
 
 Self.check = function (nbTest, object) {
   switch (nbTest) {
+  case 16:
+    if (object.comments.match(/(Non PPMI)/gim).length > 0) return 6;
+    else return 5;
+    break;
   default:
     return NaN;
   }
