decdc18daad8d9e69a30d7201ec96883bc67704a
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2023-08-28 17:27:29 +0200

    update : update ASAP PPMI rules

--- resources/rules/ASAP-PPMI/checks/datasets.js
+++ resources/rules/ASAP-PPMI/checks/datasets.js
@@ -9,7 +9,8 @@ let Self = {};
 Self.check = function (nbTest, object) {
   switch (nbTest) {
   case 16:
-    if (object.comments.match(/(Non PPMI)/gim).length > 0) return 6;
+    let match = object.comments.match(/(Non PPMI)/gim);
+    if (match && match.length > 0) return 6;
     else return 5;
     break;
   default:
