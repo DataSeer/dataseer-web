5a3a4e355bd9a8fdaadd7f977f0cbc2b49b53723
Nicolas Kieffer <kieffer.nicolas.pro@gmail.com>
2023-09-28 16:28:28 +0200

    update : add 'custom list of software' functionality (see https://www.notion.so/dataseerai/677e8ce634524d1d94b2ff99b14ab7bb?v=fe6a81b9c8e341e48162925fd83e8629&p=d9e1fc55c25a4c1192a9fb2a6b7029f9&pm=c)

---
+++ routes/api/software.js
@@ -0,0 +1,53 @@
+/*
+ * @prettier
+ */
+
+'use strict';
+
+const express = require(`express`);
+const router = express.Router();
+
+const _ = require(`lodash`);
+const async = require(`async`);
+
+const AccountsManager = require(`../../lib/accounts.js`);
+const Params = require(`../../lib/params.js`);
+const Software = require(`../../lib/software.js`);
+
+const conf = require(`../../conf/conf.json`);
+
+router.get(`/customList`, function (req, res) {
+  let accessRights = AccountsManager.getAccessRights(req.user);
+  if (!accessRights.isAdministrator) return res.status(401).send(conf.errors.unauthorized);
+  let software = req.app.get(`Software.customList`);
+  if (!Array.isArray(software)) return res.json([]);
+  return res.json(software);
+});
+
+router.post(`/customList/find`, function (req, res) {
+  let accessRights = AccountsManager.getAccessRights(req.user);
+  if (!accessRights.isAdministrator) return res.status(401).send(conf.errors.unauthorized);
+  let name = Params.convertToString(req.body.name);
+  let data = Software.findSoftwareFromCustomList(name);
+  let isError = data instanceof Error;
+  let result = isError ? data.toString() : data;
+  return res.json({
+    err: isError,
+    res: result
+  });
+});
+
+router.post(`/customList/refresh`, function (req, res, next) {
+  let accessRights = AccountsManager.getAccessRights(req.user);
+  if (!accessRights.isAdministrator) return res.status(401).send(conf.errors.unauthorized);
+  return Software.refreshCustomList(function (err, data) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(conf.errors.internalServerError);
+    }
+    req.app.set(`Software.customList`, data);
+    return res.json(req.app.get(`Software.customList`));
+  });
+});
+
+module.exports = router;
